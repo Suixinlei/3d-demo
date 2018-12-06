deecd01eaafaa6b96d8b8f3870ca0528c3ff99f1
笑斌 <xinlei.sxl@alibaba-inc.com>
2018-12-06 19:56:13 +0800

    feat: 实现了波动效果

--- src/globe/pointEarth.ts
+++ src/globe/pointEarth.ts
@@ -2,7 +2,8 @@ import * as BABYLON from 'babylonjs';
 import worldAxis from './utils/worldAxis';
 import {countries, points} from './globePoints';
 
-import * as addPoint from './utils/addPoint';
+import * as convertLngLat from './utils/convertLngLat';
+import * as geoData from './utils/geo';
 
 // Map properties for creation and rendering
 var props = {
@@ -11,7 +12,7 @@ var props = {
     width: 2048 / 2,
     height: 1024 / 2
   },
-  globeRadius: 200, // Radius of the globe (used for many calculations)
+  globeRadius: 128, // Radius of the globe (used for many calculations)
   dotsAmount: 20, // Amount of dots to generate and animate randomly across the lines
   startingCountry: 'hongkong', // The key of the country to rotate the camera to during the introduction animation (and which country to start the cycle at)
   colours: {
@@ -46,7 +47,7 @@ function returnSphericalCoordinates(latitude, longitude) {
   var targetZ = Math.sin(latitude / 180 * Math.PI) * radius;
 
   return {
-    x: targetX,
+    x: - targetX,
     y: targetY,
     z: targetZ
   };
@@ -66,6 +67,9 @@ class Earth {
   private completeNumber: number = 0;
   private globeReady: boolean = false;
 
+  private lightEffectEnable: boolean = false;
+  private lightEffectAlpha: number = 0;
+
   constructor(canvasElement: string) {
     this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
     this._engine = new BABYLON.Engine(this._canvas, true);
@@ -78,7 +82,7 @@ class Earth {
     this._camera = new BABYLON.ArcRotateCamera('camera1', Math.PI, 0, 300, BABYLON.Vector3.Zero(), this._scene);
     this._camera.lowerBetaLimit = 0.1;
     this._camera.upperBetaLimit = Math.PI;
-    this._camera.setPosition(new BABYLON.Vector3(300, 0, 0));
+    this._camera.setPosition(new BABYLON.Vector3(- 250, 0, 0));
     this._camera.attachControl(this._canvas, false);
     // this._camera.useAutoRotationBehavior = true;
     // this._camera.autoRotationBehavior.idleRotationSpeed = 0.20;
@@ -109,12 +113,13 @@ class Earth {
         latlng.y,
       );
       result.push(pointPosition);
+      particle.position = new BABYLON.Vector3(pointPosition.x, pointPosition.y, pointPosition.z);
 
-      particle.color.r = 0.5;
-      particle.color.b = 0.3;
+      particle.color = new BABYLON.Color4(1, 1, 1, 1);
 
       return particle;
     };
+    // this.earthSPS.mesh.rotation = new BABYLON.Vector3(Math.PI, Math.PI / 2, 0);
     this.earthSPS.setParticles();
     this.earthSPS.computeParticleColor = false;
 
@@ -163,18 +168,71 @@ class Earth {
 
     earthPoint.dispose();
 
-
+    const radius = 128;
     const manager = new BABYLON.GUI.GUI3DManager(this._scene);
-    const hzPoint = addPoint('杭州', this._scene, manager);
+
+    const name = 'china';
+    const xyz = returnSphericalCoordinates(countries[name].x, countries[name].y);
+    const vector = new BABYLON.Vector3(xyz.z, xyz.y, xyz.z);
+    const mesh = BABYLON.Mesh.CreateSphere(name, 16, 2, this._scene);
+    const meshMat = new BABYLON.StandardMaterial(name, this._scene);
+    meshMat.diffuseColor = new BABYLON.Color3(1, 1, 0);
+    mesh.material = meshMat;
+
+    const point = new BABYLON.GUI.MeshButton3D(mesh, `click ${name}`);
+    manager.addControl(point);
+    point.position = vector;
+
+    point.onPointerEnterObservable.add(() => {
+      meshMat.diffuseColor = new BABYLON.Color3(1, 0, 0);
+    });
+    point.onPointerOutObservable.add(() => {
+      meshMat.diffuseColor = new BABYLON.Color3(1, 1, 0);
+    });
+    point.onPointerUpObservable.add(() => {
+      // window.location.href = `/idc?name=${name}`;
+      this.lightEffect(vector);
+    });
+
+    console.log(BABYLON.Vector3.Distance(new BABYLON.Vector3(0, 0, 0), vector));
+  }
+
+  lightEffect(vector): void {
+    this.earthSPS.computeParticleRotation = false;
+    this.earthSPS.computeParticleVertex = false;
+    this.earthSPS.computeParticleTexture = false;
+    this.earthSPS.computeParticleColor = true;
+    const distance: Array<number> = [];
+    this.earthSPS.updateParticle = (particle) => {
+      if (!distance[particle.idx]) {
+        distance[particle.idx] = BABYLON.Vector3.Distance(particle.position, vector);
+      }
+      if (distance[particle.idx] < this.lightEffectAlpha && distance[particle.idx] > this.lightEffectAlpha - 30) {
+        particle.color = new BABYLON.Color4(1, 0, 0, 1);
+      } else {
+        particle.color = new BABYLON.Color4(1, 1, 1, 1);
+      }
+      return particle;
+    };
+
+    this.lightEffectAlpha = 0;
+    this.lightEffectEnable = true;
   }
 
   doRender(): void {
     this._scene.registerAfterRender(() => {
-      if (!this.globeReady) {
-        this.completeNumber = 0;
+      // if (!this.globeReady) {
+      //   this.completeNumber = 0;
+      //   this.earthSPS.setParticles();
+      //   if (this.completeNumber === this.earthSPS.nbParticles) {
+      //     this.globeReady = true;
+      //   }
+      // }
+      if (this.lightEffectEnable) {
+        this.lightEffectAlpha += 5;
         this.earthSPS.setParticles();
-        if (this.completeNumber === this.earthSPS.nbParticles) {
-          this.globeReady = true;
+        if (this.lightEffectAlpha > 256 + 30) {
+          this.lightEffectEnable = false;
         }
       }
     });
--- src/globe/utils/addPoint.js
+++ src/globe/utils/addPoint.js
@@ -4,13 +4,14 @@ import uuid from 'uuid/v1';
 import convertLngLat from './convertLngLat';
 import * as geoData from './geo';
 
+const radius = 128;
 module.exports = (name, scene, manager) => {
   const uuidNow = uuid();
-  const vector = convertLngLat(geoData[name], 128);
-  const vector3LookAt = convertLngLat(geoData[name], 150);
+  const vector = convertLngLat(geoData[name], radius);
+  const vector3LookAt = convertLngLat(geoData[name], radius + 20);
   const mesh = BABYLON.Mesh.CreateSphere(name, 16, 2, scene);
   mesh.material = new BABYLON.StandardMaterial(name, scene);
-  mesh.material.diffuseColor = new BABYLON.Color3(1, 1, 1);
+  mesh.material.diffuseColor = new BABYLON.Color3(1, 0, 1);
   mesh.lookAt(vector3LookAt);
 
   const point = new BABYLON.GUI.MeshButton3D(mesh, `click ${name}`);
